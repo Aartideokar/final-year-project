7e97f179c783e17020e1cbb237375586243fa092
Aartideokar <101311110+Aartideokar@users.noreply.github.com>
2024-04-19 16:38:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# final-year-project
+# ML_Hepatitis-B-Mortality-Prediction
+link---> https://sakshiiyadav-ml-hepatitis-b-mortality-prediction-app-vn4xn1.streamlit.app/
+## Username = sYadav   |     Password = sYadav123
---
+++ generate_keys.py
@@ -0,0 +1,14 @@
+import pickle
+from pathlib import Path
+
+import streamlit_authenticator as stauth
+
+names = ["Aarti Deokar", "Aarti"]
+usernames = ["aartideokar", "aarti"]
+passwords = ["aartideokar123", "aarti123"]
+
+hashed_passwords = stauth.Hasher(passwords).generate()
+
+file_path = Path(__file__).parent / "hashed_pws.pkl"
+with file_path.open("wb") as file:
+    pickle.dump(hashed_passwords, file)
